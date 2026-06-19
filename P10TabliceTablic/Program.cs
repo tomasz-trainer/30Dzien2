b0845ba99baab2014d4618663263d0fe1df1e772
tomasz <csharp@406-00>
2026-06-19 16:02:51 +0200

    Tablica tablic

---
+++ P10TabliceTablic/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P10TabliceTablic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // tablica jednowymiarowa
+            string[] t1d = new string[3];
+
+            // tablica dwuwymiarowa
+            string[,] t2d = new string[3, 4];
+
+            // tablica tablic
+            string[][] tTab = new string[3][];
+
+            //Wpisywanie/odczytwanie danych z/do tablicy tablic
+            tTab[1] = new string[5];
+            tTab[1][1] = "Ala";
+
+            tTab[0] = new string[3];
+            tTab[0][2] = "Ola";
+            Console.WriteLine(tTab[0][1]);
+            Console.WriteLine(tTab[1][2]);
+        }
+    }
+}
---
+++ P11ListaList/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P11ListaList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<string>> listaList = new List<List<string>>();
+            listaList.Add(new List<string>());
+            listaList.Add(new List<string>());
+
+            listaList[0].Add("Ala");
+            listaList[1].Add("Ola");
+
+            List<string[]> listaTablic;
+            List<string>[] tablicaList;
+
+            // konwersje
+            List<string> zwyklaLista = new List<string>();
+            string[] zwyklaTablica = zwyklaLista.ToArray();
+
+            zwyklaLista = zwyklaTablica.ToList();
+
+            // Inne typyt kolekcji
+            HashSet<string> zbior = new HashSet<string>();
+            Dictionary<string, string> slownik = new Dictionary<string, string>();
+            Queue<string> kolejka = new Queue<string>();
+            Stack<string> stos = new Stack<string>();
+        }
+    }
+}
