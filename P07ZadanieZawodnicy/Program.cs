3475787a0ec9108f58b7d24d1e623d9d7df174b3
tomasz <csharp@406-00>
2026-06-19 14:03:21 +0200

    rozwiązanie

--- P07ZadanieZawodnicy/Program.cs
+++ P07ZadanieZawodnicy/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,43 @@ namespace P07ZadanieZawodnicy
     {
         static void Main(string[] args)
         {
+            string pathToFile = "C:\\dane\\zawodnicy.csv";
+
+            // Wczytanie pliku do pamięci
+            string[] wiersze = File.ReadAllLines(pathToFile);
+
+            // Wczytanie nazwy kraju od użytkownika
+            Console.WriteLine("Proszę podać kraj:");
+            string kraj = Console.ReadLine();
+
+            // Praca domowa: zmodyfikować kod w taki sposób,
+            // aby indeksy kolumny imienia i nazwiska
+            // były znajdowane automatycznie
+            int indexImie = 1;
+            int indexNazwisko = 2;
+            char separator = ';';
+            List<string> zawodnicyDanegoKraju = new List<string>();
+            foreach (var zawodnik in wiersze)
+            {
+                if (zawodnik.Contains(kraj))
+                {
+                    string[] daneZawodnika = zawodnik.Split(separator);
+                    string imieNazwisko = $"{daneZawodnika[indexImie]} {daneZawodnika[indexNazwisko]}";
+                    zawodnicyDanegoKraju.Add(imieNazwisko);
+                }
+            }
+
+            string sciezkaFolderuWyjsciowego = $"C:\\dane\\kraje";
+            if (!Directory.Exists(sciezkaFolderuWyjsciowego))
+            {
+                Directory.CreateDirectory(sciezkaFolderuWyjsciowego);
+            }
+
+            string sciezkaPlikuWyjsciowego = sciezkaFolderuWyjsciowego + $"\\{kraj}.txt";
+            if (!File.Exists(sciezkaPlikuWyjsciowego))
+            {
+                File.WriteAllLines(sciezkaPlikuWyjsciowego, zawodnicyDanegoKraju);
+            }
         }
     }
 }
